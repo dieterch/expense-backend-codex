c6df91ee93d8ed9481603bb331c2dca47b0142cf
dieterch <dieter.chvatal@gmail.com>
2026-03-27 18:31:05 +0100

    Load MDI font icons in the web app

--- web/app/plugins/vuetify.ts
+++ web/app/plugins/vuetify.ts
@@ -1,5 +1,6 @@
+import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify } from "vuetify";
-import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
